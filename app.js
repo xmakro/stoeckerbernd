5c0b82cf8ceaac904a62b53735eeb66d9ad5d81c
Manuel Kroiss <manuel@makrolabs.org>
2017-06-25 15:39:47 +0100

    Refactor to vanilla JS and BaguetteBox

---
+++ app.js
@@ -0,0 +1,53 @@
+function load() {
+    let path = location.hash.substr(1);
+    if (!path) path = 'start';
+    let args = path.split('/');
+
+    // Stamp content.
+    let content = document.querySelector('#content');
+    if (args[0] == 'gallerie') {
+      let name = args[args.length-1];
+      if (name == 'werke') name = 'Skulpturen';
+      content.innerHTML = gallery(data[name]);
+      baguetteBox.run('.gallery', {
+          noScrollbars: true,
+          animation: false,
+      });
+    } else {
+      let template = document.querySelector('#' + path);
+      let clone = document.importNode(template.content, true);
+      content.innerHTML = '';
+      content.appendChild(clone);
+    }
+
+    // Update links and sub menus.
+    for (let e of document.querySelectorAll('.active')) {
+      e.classList.remove('active');
+    }
+    for (let e of document.querySelectorAll('a')) {
+      if (e.getAttribute('href') == '#' + path) {
+        e.classList.add('active');
+      }
+    }
+    for (let e of document.querySelectorAll('[data-path]')) {
+      if (path.startsWith(e.getAttribute('data-path'))) {
+        e.classList.add('active');
+      }
+    }
+}
+
+function gallery(doc) {
+  let images = doc.images.map(item => {
+    let height = item.height / item.width * 180;
+    return `<a href="images/gallery-full/${item.file}" title="${item.title}: ${item.description}">
+      <div>
+        <img src="images/gallery-thumb/${item.file}.png" width="188" height="${height}">
+        <span>${item.title}</span>
+      </div>
+    </a>`
+  }).join('');
+  return `<h1>${doc.title}</h1><div class="gallery">${images}</div>`;
+}
+
+window.onhashchange = load;
+window.onload = load;
---
+++ data.js
@@ -0,0 +1,494 @@
+var data = {
+  "aktuelles": {
+    "title": "Aktuelles",
+    "images": [{
+      "file": "20gips.jpg",
+      "title": "Ausstellung vom 25.06 bis 21.08 in Neuöttting",
+      "description": "Stadtmuseum Neuöttting\nBernd Stöcker\nBronzezeit\nEröffnung am 24.Juni 2016\nAustellungsdauer vom 25.Juni bis 21. August",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "zeitungslesende.jpg",
+      "title": "Ausstellung vom 15.09 bis 16.10 in Passau",
+      "description": "Kunstverein Passau\nin der Sankt Anna- Kapelle\nHeiliggeistgasse 4, 94032 Passau\nBernd Stöcker Skulpturen und Iris Schaarschmidt Malerei\nEröffnung am 15. September 2016\nAusstellungsdauer vom 16. September bis 16. Oktober 20016",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "iris.jpg",
+      "title": "Ausstellung vom 15.09 bis 16.10 in Passau",
+      "description": "Kunstverein Passau\nin der Sankt Anna- Kapelle\nHeiliggeistgasse 4, 94032 Passau\nBernd Stöcker Skulpturen und Iris Schaarschmidt Malerei\nEröffnung am 15. September 2016\nAusstellungsdauer vom 16. September bis 16. Oktober 20016",
+      "width": "448",
+      "height": "321"
+    }]
+  },
+  "entstehung": {
+    "title": "Entstehung",
+    "images": [{
+      "file": "01geruest.jpg",
+      "title": "Stahlger\u00fcst",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "02geruest.jpg",
+      "title": "Stahlger\u00fcst",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "03tonmodell.jpg",
+      "title": "Tonmodell",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "04tonmodell.jpg",
+      "title": "Tonmodell",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "05tonmodell.jpg",
+      "title": "Fertiges Tonmodell",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "06-1tonmodell.jpg",
+      "title": "Fertiges Tonmodell",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "06tonmodell.jpg",
+      "title": "Fertiges Tonmodell",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "07gips.jpg",
+      "title": "Vorbereitung der Gipsnegativschale",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "08gips.jpg",
+      "title": "Gipsnegativschale",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "09gips.jpg",
+      "title": "Gipsnegativschale ",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "10gips.jpg",
+      "title": "Gipsnegativschale",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "11gips.jpg",
+      "title": "Gipsnegativschale",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "12gips.jpg",
+      "title": "Abnehmen der Gipsnegativschale",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "13gips.jpg",
+      "title": "Gipsguss",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "14gips.jpg",
+      "title": "Wegschlagen der Gipsnegativschale",
+      "description": "(verlorene Form)",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "15gips.jpg",
+      "title": "Wegschlagen der Gipsnegativschale",
+      "description": "(verlorene Form)",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "16gips.jpg",
+      "title": "Wegschlagen der Gipsnegativschale",
+      "description": "(verlorene Form)",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "17gips.jpg",
+      "title": "Wegschlagen der Gipsnegativschale",
+      "description": "(verlorene Form)",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "18-5gips.jpg",
+      "title": "Wegschlagen der Gipsnegativschale",
+      "description": "(verlorene Form)",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "18gips.jpg",
+      "title": "Fertiger Gipsguss",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "19gips.jpg",
+      "title": "Bronzerohguss",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "20gips.jpg",
+      "title": "Patinieren des Bronzegusses",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "22fertig.jpg",
+      "title": "Fertige Figur in Hamburg-Poppenb\u00fcttel",
+      "description": "",
+      "width": "500",
+      "height": "535"
+    }]
+  },
+  "grafiken": {
+    "title": "Grafiken",
+    "images": [{
+      "file": "bunker1.jpg",
+      "title": "See und Bunker",
+      "description": "25.5.1983, Gr\u00f6\u00dfe 25,6 x 39 cm\nAus der Radierfolge \"Herzliche Urlaubsgr\u00fc\u00dfe von der Westfront!\"",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "bunker2.jpg",
+      "title": "Bunker mit Leuchtfeuer",
+      "description": "15.5.1983, Radierung, 27 x 41 cm\nAus der Radierfolge \"Herzliche Urlaubsgr\u00fc\u00dfe von der Westfront!\"",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "bunker3.jpg",
+      "title": "Landschaft mit Bunker",
+      "description": "1983, Radierung, 29,5 x 39,5 cm\nAus der Radierfolge \"Herzliche Urlaubsgr\u00fc\u00dfe von der Westfront!\"",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "hockende.jpg",
+      "title": "Cavtat",
+      "description": "7.7.1988, Sepiatusche, 32,5 x 52 cm",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "liegende.jpg",
+      "title": "Schwangere",
+      "description": "2.7.1989, Tusche gebrannte Siena, 26,5 x 38 cm",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "linolschnitt.jpg",
+      "title": "Zwei Frauen",
+      "description": "2000, Linolschnitt",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "linolschnitt2.jpg",
+      "title": "Am Strand",
+      "description": "12.6.2000, Linolschnitt",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "linolschnitt3.jpg",
+      "title": "Steintorso mit Stuhl",
+      "description": "1.9.1999, Linolschnitt",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "salon.jpg",
+      "title": "Im Salon",
+      "description": "17.5.1983, Radierung, 21 x 28 cm\nAus der Radierfolge \"Herzliche Urlaubsgr\u00fc\u00dfe von der Westfront!\"",
+      "width": "500",
+      "height": "535"
+    }]
+  },
+  "skulpturen": {
+    "title": "Skulpturen",
+    "images": [{
+      "file": "christopherus.jpg",
+      "title": "Christopherus",
+      "description": "2002, Gips, Werkstattphoto\n\nFoto: ",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "elvis.jpg",
+      "title": "Elvis Presley",
+      "description": "1978-79, Muschelkalk, H\u00f6he 205cm\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "frauenundgaskammer.jpg",
+      "title": "Frauen und Gaskammer",
+      "description": "1982, Muschelkalk und Mamor, H\u00f6he 30,5cm\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "frierende.jpg",
+      "title": "Frierende",
+      "description": "1978, Muschelkalk, H\u00f6he 160 cm\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "grosseliegende.jpg",
+      "title": "Gro\u00dfe Liegende",
+      "description": "1990, wei\u00dfer Carraramarmor\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "haarewaschen2.jpg",
+      "title": "Sich Haarewaschende",
+      "description": "1987, Zustand in Ton, Werkstattfoto\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "heine.jpg",
+      "title": "Heinrich Heine",
+      "description": "1985, Bardiglio, H\u00f6he 98 cm\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "heinekopf2.jpg",
+      "title": "Kopf Heinrich Heine",
+      "description": "1985, Bardiglio, H\u00f6he 20 cm\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "hose.jpg",
+      "title": "Torso mit KZ-Hose",
+      "description": "1984-89, Muschelkalk, H\u00f6he 193 cm\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "ingridgesicht.jpg",
+      "title": "Ingrid",
+      "description": "1981, Bleiguss, H\u00f6he 80 cm\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "julius.jpg",
+      "title": "Julius Sch\u00e4tzle",
+      "description": "1989, wei\u00dfer Carraramarmor, H\u00f6he 60 cm\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "liegender.jpg",
+      "title": "Liegender",
+      "description": "1985, Bardiglio, L\u00e4nge 42 cm\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "liegender2.jpg",
+      "title": "Liegender mit Decke",
+      "description": "1987, Muschelkalk, L\u00e4nge 224 cm\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "magdalena.jpg",
+      "title": "Magdalena",
+      "description": "1999, Gips f\u00fcr Bronze, H\u00f6he 104 cm\n\nFoto: Prof. Ch. H\u00f6pfner",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "munitionsfabrik.jpg",
+      "title": "Munitionsfabrik",
+      "description": "1980-81, Kalkstein Schupbach Schwarz, H\u00f6he 238 cm\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "schwangere.jpg",
+      "title": "Schwangere",
+      "description": "1989-90, Gips, L\u00e4nge 171 cm\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "sonnenbadende.jpg",
+      "title": "Sonnenbadende",
+      "description": "1985, Muschelkalk, H\u00f6he 55 cm, Privatbesitz\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }]
+  },
+  "oeffentlich": {
+    "title": "Öffentlich",
+    "images": [{
+      "file": "Brunnen.jpg",
+      "title": "Vesper am Brunnen",
+      "description": "2010, Salzburger Marmor und roter schwedischer Granit",
+      "width": "1000",
+      "height": "1350"
+    }, {
+      "file": "christopherus2.jpg",
+      "title": "Christopherus",
+      "description": "2002, Bronze, auf der Neckarbr\u00fccke zwischen Ziegelhausen und Schlierbach, Heidelberg\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "claudius1.jpg",
+      "title": "Der Freudensprung",
+      "description": "Matthias-Claudius-Denkmal, 2001, Bronze, auf dem Wandsbeker Marktplatz, Hamburg, (Literatur: Bernd St\u00f6cker, Wandsbeker Skulpturen, M\u00fchlenbek-Verlag, 2001, Seite 18)\n\nFoto: Axel St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "claudius2.jpg",
+      "title": "Der Freudensprung",
+      "description": "Matthias-Claudius-Denkmal, 2001, Bronze, auf dem Wandsbeker Marktplatz, Hamburg, (Literatur: Bernd St\u00f6cker, Wandsbeker Skulpturen, M\u00fchlenbek-Verlag, 2001, Seite 18)\n\nFoto: M. Rohdach",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "claudiusollenfelde.jpg",
+      "title": "Matthias Claudius",
+      "description": "1990, Bronze, Wollinerstra\u00dfe 98, Hamburg-Rahlstedt, (Literatur: Bernd St\u00f6cker, Wandsbeker Skulpturen, M\u00fchlenbek-Verlag, 2001, Seite 34)\n\nFoto: Axel St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "evaraalsteet.jpg",
+      "title": "Eva",
+      "description": "1979\/80, Bronze, H\u00f6he 203 cm, Ortsamt, Rahlstedter Stra\u00dfe, Hamburg-Rahlstedt\n\nFoto: Axel St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "evastuttgart.jpg",
+      "title": "Eva",
+      "description": "1984, Bronze, H\u00f6he 203 cm, Uhlandsh\u00f6he, Stuttgart\n\nFoto: R. Truckenm\u00fcller",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "ikarus.jpg",
+      "title": "Ikarus",
+      "description": "1994, Bronze, Gewerbepark S\u00fcd, Puchheim bei M\u00fcnchen\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "merkur.jpg",
+      "title": "Merkurs\u00e4ule",
+      "description": "1993, H\u00f6he 20 m, Bronze und eingef\u00e4rbter Stahlbeton, Merkurpark, Hamburg-Rahlstedt, (Literatur: Wandsbecker Skulpturen, Seite 26)\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "mutterundkind.jpg",
+      "title": "Mutter und Kind",
+      "description": "1993, Bronze, Maxstra\u00dfe\/Auenstra\u00dfe, Hamburg Eilbeck (Werkstattfoto)\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "mutterundkind2.jpg",
+      "title": "Mutter und Kind",
+      "description": "1993, Bronze, Maxstra\u00dfe\/Auenstra\u00dfe, Hamburg Eilbeck\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "vesper.jpg",
+      "title": "Vesper im Weinberg",
+      "description": "2004, Bronze, Verkehrskreisel am H\u00e4usle\u00e4cker, Wiesloch, (Werkstattfoto im Tonzustand)\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "wernerotto.jpg",
+      "title": "Werner Otto",
+      "description": "2009, Bronze, Heegbarg 30, Hamburg Poppenb\u00fcttel\n\nFoto: B. St\u00f6cker",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "zeitungslesende.jpg",
+      "title": "Zeitungslesende am Strand",
+      "description": "1999, Bronze, Wandsbeker Marktplatz, Hamburg\n\nFoto: J. Weber",
+      "width": "500",
+      "height": "535"
+    }]
+  },
+  "sakral": {
+    "title": "Sakral",
+    "images": [{
+      "file": "altarheilbronn.jpg",
+      "title": "Altar",
+      "description": "1996, Eiche, Auferstehungskirche Heilbronn-B\u00f6ckingen",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "giengenchristus.jpg",
+      "title": "Christus",
+      "description": "1988, Bardiglio, H\u00f6he 280 cm, Heiliggeistkirche, Giengen an der Brenz",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "herzzentrum.jpg",
+      "title": "Altar",
+      "description": "1996, Wei\u00dfer Carraramarmor, \u00d6kumenische Kapelle im Deutschen Herzzentrum M\u00fcnchen, Lazarettstra\u00dfe M\u00fcnchenmitte",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "herzzentrum2.jpg",
+      "title": "Marienfigur",
+      "description": "1996, Muschelkalk, \u00d6kumenische Kapelle im Deutschen Herzzentrum M\u00fcnchen, Lazarettstra\u00dfe M\u00fcnchen-Mitte",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "hlsebastian.jpg",
+      "title": "Heiliger Sebastian",
+      "description": "2008, Calcaire Jaune, Balsbach im Odenwald",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "lukas.jpg",
+      "title": "Lukasfigur",
+      "description": "1989, Bardiglio, Lukaskirche, Hauffstra\u00dfe 60, Schw\u00e4bisch Hall",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "johannesbrunnen.jpg",
+      "title": "Johannesbrunnen",
+      "description": "2001, Untersberger und roter Salzburger Marmor, Neukirchen, Niederbayern",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "neuhermsheim.jpg",
+      "title": "Altar",
+      "description": "1991, wei\u00dfer Carraramarmor, Kath. Gemeindezentrum Maria-K\u00f6nigin, Mannheim-Neuhermsheim, (Literatur: Das M\u00fcnster 3\/94, Seite 206-208), Altarglasfenster von Raphael Seitz",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "sthildegard.jpg",
+      "title": "Christus",
+      "description": "1998-99,Wei\u00dfer Carraramarmor, St Hildegardkirche, D\u00fcrkheimerstra\u00dfe 88, Mannheim-K\u00e4fertal",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "ziegelhausen2.jpg",
+      "title": "Altar",
+      "description": "1997, Jerusalemmarmor, St Laurentius, Heidelberg-Ziegelhausen, M\u00fchlweg 11",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "ziegelhausen.jpg",
+      "title": "Ambo mit Eliasfigur",
+      "description": "1997, Jerusalemmarmor, St Laurentius, Heidelberg-Ziegelhausen, M\u00fchlweg 11",
+      "width": "500",
+      "height": "535"
+    }, {
+      "file": "ziegelhausen3.jpg",
+      "title": "Kreuzwegstation",
+      "description": "2003, Jerusalemmarmor, St Laurentius, Heidelberg-Ziegelhausen, M\u00fchlweg 11",
+      "width": "500",
+      "height": "535"
+    }]
+  }
+};
